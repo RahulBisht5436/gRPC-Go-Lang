1a1730d96089dd7d35fdde0ed98ca8af7f4f5371
Rahul bisht <rahul.bisht@telgoo5.com>
2026-05-30 12:34:58 +0530

    changes for the basic Server and Client Config

--- gRPC project/greet/client/main.go
+++ gRPC project/greet/client/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var addr string = "localhost:50051"
+
+func main() {
+	// this disables the SSL for now as it will through error if not setted to insecure
+	connc, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic("Not able to start the server")
+	}
+
+	defer connc.Close()
+}
